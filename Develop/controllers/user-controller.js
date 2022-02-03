5f332cd3c8d8ef83dcf41c06a9c9e615a62d657d
MERYEM-AD <adiri.meryem.mw@gmail.com>
2022-02-02 20:29:51 -0500

    Update user  by ID  was done successfully

--- Develop/controllers/user-controller.js
+++ Develop/controllers/user-controller.js
@@ -36,6 +36,19 @@ const userController = {
         .catch((err) => res.status(400).json(err));
     },
 
+  // update user by id
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, { new: true })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "Sorry , Unfound User !!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+
 };
 
 module.exports = userController;
--- Develop/routes/api/user-routes.js
+++ Develop/routes/api/user-routes.js
@@ -4,12 +4,13 @@ const {
   getAllUsers,
   getUserById,
   createUser,
+  updateUser
 } = require("../../controllers/user-controller");
 
 // get route => /api/users || post routes => /api/users
 router.route("/").get(getAllUsers).post(createUser)
 
-// get one user by id route  => /api/users/:id 
-router.route("/:id").get(getUserById)
+// get one user by id route  => /api/users/:id  || update user by id route =>api/users/:id 
+router.route("/:id").get(getUserById).put(updateUser)
 
 module.exports = router;
