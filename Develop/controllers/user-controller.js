120f15481e9eb6bccee657df3961380883787f32
MERYEM-AD <adiri.meryem.mw@gmail.com>
2022-02-02 20:33:47 -0500

    Delete user by ID  was done successfully

--- Develop/controllers/user-controller.js
+++ Develop/controllers/user-controller.js
@@ -36,7 +36,7 @@ const userController = {
         .catch((err) => res.status(400).json(err));
     },
 
-  // update user by id
+  // update user
   updateUser({ params, body }, res) {
     User.findOneAndUpdate({ _id: params.id }, body, { new: true })
       .then((dbUserData) => {
@@ -48,6 +48,19 @@ const userController = {
       })
       .catch((err) => res.status(400).json(err));
   },
+
+  // delete user
+    deleteUser({ params }, res) {
+      User.findOneAndDelete({ _id: params.id })
+        .then((dbUserData) => {
+          if (!dbUserData) {
+            res.status(404).json({ message: "Sorry , Unfound User !!" });
+            return;
+          }
+          res.json(dbUserData);
+        })
+        .catch((err) => res.status(400).json(err));
+    },
 
 };
 
--- Develop/routes/api/user-routes.js
+++ Develop/routes/api/user-routes.js
@@ -4,13 +4,14 @@ const {
   getAllUsers,
   getUserById,
   createUser,
-  updateUser
+  updateUser,
+  deleteUser
 } = require("../../controllers/user-controller");
 
 // get route => /api/users || post routes => /api/users
 router.route("/").get(getAllUsers).post(createUser)
 
-// get one user by id route  => /api/users/:id  || update user by id route =>api/users/:id 
-router.route("/:id").get(getUserById).put(updateUser)
+// get one user by id route  => /api/users/:id  || update user by id route =>api/users/:id || delete user by id route =>api/users/:id
+router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser)
 
 module.exports = router;
